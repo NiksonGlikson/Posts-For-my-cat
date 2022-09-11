6ca3d1cc01e01560be1496dcba1eefd5e483ec75
Никита Глинкин <hermesshant@gmail.com>
2022-09-11 17:17:00 +0300

    fix, add api json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,24 +6,38 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./components/hooks/UsePosts";
+import PostService from '../src/components/Api/PostService';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "My cat", body: "Best of the best" },
-    { id: 2, title: "My cat", body: "Beautiful" },
-    { id: 3, title: "My cat", body: "Gentle" },
-    { id: 4, title: "My cat", body: "Sweet" },
-    { id: 5, title: "My cat", body: "Likes to play" },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false)
+
+  useEffect(() => {
+    fetchPosts()
+    return () => {
+      
+    };
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
+
+  // Обращаемся к бесп серверу и получаем массив постов
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    setTimeout(async () => {
+      const posts = await PostService.getAll()
+      setPosts(posts);
+      setIsPostsLoading(false);
+    }, 1000)
+  }
+
   // Получим пост из дочернего компонента
   const deletePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -39,11 +53,14 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        delete={deletePost}
-        posts={sortedAndSearchedPosts}
-        title="Compliments to my future cat"
-      />
+      {isPostsLoading
+        ? <h1>is loading...</h1>
+        : <PostList
+          delete={deletePost}
+          posts={sortedAndSearchedPosts}
+          title="Compliments to my future cat"
+          />
+    }
     </div>
   );
 }
